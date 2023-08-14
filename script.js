f1b66653afdd87865095ae7e66f80287c0e295e8
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-08-14 00:53:39 -0700

    Update_the_JS_Code

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ console.log(newArr);
 
 const numbers = "John, John, Prince";
 let split = numbers.split(", ").console.log(moreNumbers);
+// let names = "jack , jack , prince";: 
+// This line initializes a string variable names with the value "jack , jack , prince".
+// let newArr = names.split(", ")
+// ((name) => {...});: Here, you're trying to split the names string into an array using the delimiter ", " .
+// (comma followed by a space). However, there's a mistake in the syntax. 
+// The correct usage should involve the .map() function for transformation after the split.
