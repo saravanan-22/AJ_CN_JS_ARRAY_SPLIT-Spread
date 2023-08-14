cea183cf9c340cafb83566645dee1d7542a22c73
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-08-14 00:50:32 -0700

    JS_Class_Notes

---
+++ script.js
@@ -0,0 +1,9 @@
+let names = "jack , jack , prince";
+
+let newArr = names.split(", ")((name) => {
+  return { ...name, newname: "Arun" };
+});
+console.log(newArr);
+
+const numbers = "John, John, Prince";
+let split = numbers.split(", ").console.log(moreNumbers);
